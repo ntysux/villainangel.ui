ebcb65408acdcc75ed9483cefd5f2230ced37a92
Duong Quoc Viet <ntysux@gmail.com>
2023-12-31 23:26:48 +0700

    create & using: button

---
+++ app/button/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <button className="px-11 py-7 rounded-lg text-sm text-neutral-400 hover:text-neutral-600 shadow-lg shadow-neutral-200 hover:shadow transition-all duration-300">
+      Bắt đầu
+    </button>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={quicksand.className}>
-        {children}
+        <div className='min-h-screen flex items-center justify-center'>
+          {children}
+        </div>
       </body>
     </html>
   )
