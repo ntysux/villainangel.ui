1926cdc75f8b2bb679cace020c3838677274253b
Duong Quoc Viet <ntysux@gmail.com>
2024-01-02 17:33:46 +0700

    create & using: switch ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link"
 
 export default function Page() {
   return (
-    <>
+    <div className="grid grid-cols-3 gap-3">
       <Link href='/button'>Button</Link>
-    </>
+      <Link href='/switch'>Switch</Link>
+    </div>
   )
 }
---
+++ app/switch/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+
+export default function Page() {
+  const [enabled, setEnabled] = useState(false)
+
+  return (
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      className={`
+        ${enabled ? 'bg-neutral-700' : 'bg-neutral-300'}
+        outline-none flex py-0.5 px-2.5 rounded-full transition-colors duration-300
+      `}
+    >
+      <span
+        className={`
+          ${enabled ? 'translate-x-2' : '-translate-x-2'}
+          p-2 rounded-full bg-white transform transition-transform duration-300
+        `}
+      />
+    </Switch>
+  )
+}
